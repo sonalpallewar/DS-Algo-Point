6bc89610f95d1e31a9d04196691f8f71cd40f087
sonalpallewar <30591977+sonalpallewar@users.noreply.github.com>
2024-06-06 18:35:50 +0530

    Update Binary_Search.py

--- Python/Binary_Search.py
+++ Python/Binary_Search.py
@@ -1,4 +1,28 @@
 # Binary Search = An efficent way to search in a sorted list
+def binary_new_function():
+    print("NEW function in binary search")
+     while low < high:
+        mid = low + (high-1) // 2
+        if(arr[mid] == x):
+            return mid
+        elif(arr[mid] <x):
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    return -1
+
+print('Enter a list of number')
+arr = list(map(int,input().split()))
+n=len(arr)
+
+arr.sort()
+x =int(input("Enter the number to find\n"))
+result = binary(arr,0,len(arr)-1,x)
+if(result == -1):
+    print("Element not found")
+else:
+    print("Element found at ",result+1," position")
 def binary(arr,low,high,x):
     while low < high:
         mid = low + (high-1) // 2
